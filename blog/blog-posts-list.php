efa013f3464e7c684502900d20bd8907f3d51d8d
John Zastrow <br8kwall@gmail.com>
2021-02-18 16:17:31 -0500

    Minor edits

--- blog/blog-posts-list.php
+++ blog/blog-posts-list.php
@@ -1,13 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Blog title</title>
+    <title>GeoNotes</title>
     <link rel="stylesheet" href="blog.css">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
+<h2>Like my sticky notes on the web</h2>
     <div class="blog">
+
+    <!-- Code to render list. The only thing we need -->
         <?php include 'postListRenderer.php'; ?>
+    <!-- end code -->   
     </div>
 </body>
 </html>
