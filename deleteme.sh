e6d31072ded419049b63003d6ab6794af9de6ebc
John Zastrow <johnzastrow@Marisas-MacBook-Pro.local>
2021-02-20 22:18:12 -0500

    shell file

---
+++ deleteme.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo “hi”
+
+title: / # 
+
+remove line starting "author: John C. Zastrow"
+same for type: post
+date: 2008-07-06T01:17:21+00:00
+same for url: /2008/07/05/automate-your-life/
